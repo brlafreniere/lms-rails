1b790486a0c738a9a1812ae02bf43d83bc17bde5
Blaine Lafreniere <blaine@lizardgizzards.com>
2019-10-19 10:00:51 +0000

    Add admin flag to user
    Plus some branch stuff...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     if user && user.authenticate(params[:password])
       token = JWTTranscoder.encode(user_id: user.id)
       time = Time.now + 24.hours.to_i
-      render json: { token: token, expiration: time.strftime("%m-%d-%Y %H:%M") }, status: :ok
+      render json: { token: token, expiration: time.strftime("%m-%d-%Y %H:%M"), user: user.as_json(except: :password_digest)}, status: :ok
     else
       render json: { error: 'Unauthorized' }, status: :unauthorized
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_16_065649) do
+ActiveRecord::Schema.define(version: 2019_10_18_034707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "branches", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_branches_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
